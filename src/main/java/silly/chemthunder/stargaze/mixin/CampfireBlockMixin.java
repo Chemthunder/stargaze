591d7a50c478616ad3a3f1b80d9e75e705e275cc
Chemthunder <chemthunderisme@gmail.com>
2025-10-13 19:00:37 -0400

    a

---
+++ src/main/java/silly/chemthunder/stargaze/mixin/CampfireBlockMixin.java
@@ -0,0 +1,32 @@
+package silly.chemthunder.stargaze.mixin;
+
+import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCollisionHandler;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CampfireBlock.class)
+public abstract class CampfireBlockMixin extends BlockWithEntity implements Waterloggable {
+    protected CampfireBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(method = "onEntityCollision", at = @At("HEAD"))
+    private void stargaze$astralShenanigans(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, CallbackInfo ci) {
+        BlockPos belowPos = pos.down();
+
+        if (world.getBlockState(belowPos).isOf(Blocks.SOUL_SAND) && entity instanceof PlayerEntity player) {
+            if (player != null) {
+                player.sendMessage(Text.literal("a").formatted(Formatting.DARK_PURPLE), true);
+            }
+        }
+    }
+}
