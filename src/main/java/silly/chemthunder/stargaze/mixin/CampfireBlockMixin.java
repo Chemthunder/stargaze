dad189d4ef7d20c1d3dcf5907809e8d230611b6d
Chemthunder <chemthunderisme@gmail.com>
2025-10-15 13:56:54 -0400

    a

--- src/main/java/silly/chemthunder/stargaze/mixin/CampfireBlockMixin.java
+++ src/main/java/silly/chemthunder/stargaze/mixin/CampfireBlockMixin.java
@@ -4,9 +4,11 @@ import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCollisionHandler;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,14 +21,25 @@ public abstract class CampfireBlockMixin extends BlockWithEntity implements Wate
         super(settings);
     }
 
-    @Inject(method = "onEntityCollision", at = @At("HEAD"))
-    private void stargaze$astralShenanigans(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, CallbackInfo ci) {
+//    @Inject(method = "onEntityCollision", at = @At("HEAD"))
+//    private void stargaze$astralShenanigans(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, CallbackInfo ci) {
+//        BlockPos belowPos = pos.down();
+//
+//        if (world.getBlockState(belowPos).isOf(Blocks.SOUL_SAND) && entity instanceof PlayerEntity player) {
+//            if (player != null) {
+//                player.sendMessage(Text.translatable("text.stargaze.campfire_attack").formatted(Formatting.BLACK), true);
+//            }
+//        }
+//    }
+
+
+    @Override
+    protected void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         BlockPos belowPos = pos.down();
 
-        if (world.getBlockState(belowPos).isOf(Blocks.SOUL_SAND) && entity instanceof PlayerEntity player) {
-            if (player != null) {
-                player.sendMessage(Text.translatable("text.stargaze.campfire_attack").formatted(Formatting.BLACK), true);
-            }
+        if (world.getBlockState(belowPos).isOf(Blocks.SOUL_SAND)) {
+
         }
+        super.randomTick(state, world, pos, random);
     }
 }
